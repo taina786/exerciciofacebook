f251d3e68d23aedbf191df50e10c40b5512c1187
taina786 <165830476+taina786@users.noreply.github.com>
2024-04-02 16:37:12 -0300

    atividade do fecebook

---
+++ dadosUsuario.js
@@ -0,0 +1,13 @@
+user = {
+    'nome':'Dean muito Lindo Winchester',
+    'informação': 'fiz a serie Sobrenatural',
+    'sexo': 'Masculino',
+    'DataDeNascimento': ' 24 de janeiro de 1979',
+    'pais': 'John winchester e Mary whinchester',
+    'irmãos': 'Sammuel winchester',
+    'cidade': 'Nova York',
+    'cidadenatal': 'Dallas, Texas',
+    'bio': 'solteiro, em busca de um amor',
+    'frasefavorita': 'matar demônio é o lema da familia',
+    'photo':"https://i.pinimg.com/736x/4e/9e/45/4e9e45af19f5dd6bb7476e9769d79f32.jpg",
+}
---
+++ funcoes.js
@@ -0,0 +1,32 @@
+localFoto = document.getElementById('fotododitocujo');
+localFoto.src = user.photo;
+
+localNome = document.getElementById('nomeDoMeliante');
+localNome.innerHTML = user.nome;
+
+localInformação = document.getElementById('informaçãoDoMeliante');
+localInformação.innerHTML = user. informação;
+
+localsexo = document.getElementById('sexoDoMeliante');
+localsexo.innerHTML = user. sexo;
+
+localDataDeNascimento = document.getElementById('DataDeNascimentoDoMeliante');
+localDataDeNascimento.innerHTML = user. DataDeNascimento;
+
+localpais = document. getElementById('paisdomeliante');
+localpais.innerHTML = user. pais;
+
+localirmãos  = document. getElementById('irmãosdomeliante');
+localirmãos.innerHTML = user. irmãos;
+
+localcidade = document. getElementById('cidadedomeliante');
+localcidade.innerHTML = user. cidade;
+
+localcidadenatal = document. getElementById('cidadenataldomeliante'),
+localcidadenatal.innerHTML = user. cidadenatal;
+
+localbio = document. getElementById('biodomeliante');
+localbio.innerHTML = user. bio;
+
+localfrasefavorita = document. getElementById('frasefavoritadomeliante');
+localfrasefavorita.innerHTML = user. frasefavorita;
